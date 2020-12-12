e5180ca57d2dd739001a624bb6ac478ba5c857d8
cherry <2634667456@qq.com>
2020-12-12 21:49:13 +0800

    this is first file

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: cherry
+	> Mail: 2634667456@qq.com 
+	> Created Time: Sat Dec 12 21:46:31 2020
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello world" << endl;
+
+	return 0;
+}
